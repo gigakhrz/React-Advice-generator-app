06a4532f320758c2812fbce7df2a4569b87ded0d
gigakhrz <kharazishvilig13@gmail.com>
2023-05-05 09:28:53 +0400

    create all the main files

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import GlobalStyles from "./GlobalStyles";
+import Main from "./components/Main";
+
+function App(): JSX.Element {
+  return (
+    <div>
+      <GlobalStyles />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main.tsx
@@ -0,0 +1,7 @@
+import { useState, useEffect } from "react";
+
+const Main = (): JSX.Element => {
+  return <div></div>;
+};
+
+export default Main;
